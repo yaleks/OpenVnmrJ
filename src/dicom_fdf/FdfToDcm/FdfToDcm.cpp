987429cb27a345df73ed1e63c766b6a53586cdbf
yaleks <yaleks@gmail.com>
2026-07-09 14:36:10 -0400

    Update FdfToDcm.cpp - fix UB

--- src/dicom_fdf/FdfToDcm/FdfToDcm.cpp
+++ src/dicom_fdf/FdfToDcm/FdfToDcm.cpp
@@ -73,9 +73,9 @@ int main(int argc,char **argv)
 	char				customtagPath[PATH_MAX];
 	char				inputfilename[PATH_MAX];
 	char				outdcmfilename[PATH_MAX];
-	char				uidSuffix[64];
-	char				studyUID[128];
-	char				seriesUID[128];
+	char				uidSuffix[PATH_MAX];
+	char				studyUID[256];
+	char				seriesUID[PATH_MAX + 64];
 	char 				*pixelBuf;
         char                            syscmd[256];
         char                            stdid[256];
@@ -116,7 +116,7 @@ int main(int argc,char **argv)
 
 	struct dirent 		**namelist;
 	int 				nFiles;
-	FILE				*fd, *procparfd, *customfd, *fdf_fd, *fsha;
+	FILE				*fd, *procparfd, *customfd, *fdf_fd = NULL, *fsha;
 
 	GetNamedOptions 		options(argc,argv);
 	InputOptions			input_options(options);
@@ -210,7 +210,7 @@ int main(int argc,char **argv)
 
 	//UI Series Instance UID 	 VR=<UI>   VL=<0x0032>
 	//Example: <1.3.6.1.4.1.670589.1.3.0.123.127440.31032.8323329>
-	sprintf(seriesUID, "1.3.6.1.4.1.%s.1.3.0.123.%s", VARIAN_COMPANY_ID, uidSuffix);
+	snprintf(seriesUID, sizeof(seriesUID), "1.3.6.1.4.1.%s.1.3.0.123.%s", VARIAN_COMPANY_ID, uidSuffix);
 
 	while(nFiles--)
 	{
@@ -419,6 +419,11 @@ int main(int argc,char **argv)
 		else
 		{
 			ProcessFidData(fd, list, &fdfValues);
+			// Preserve the descriptor so the shared code below
+			// (ProcessCustomTags / fclose) has a valid FILE*,
+			// matching what the isfdf branch does.
+			fdf_fd = fd;
+			fd = NULL;
 		}
 		
 		// Setup the variables used to process the individual tags from the basic DICOM
@@ -540,7 +545,7 @@ int main(int argc,char **argv)
 
 		//sprintf(studyUID, "1.3.6.1.4.1.%s.1.2.0.%d", stdid, (unsigned int)gethostid());
 	
-		 sprintf(studyUID, "1.3.6.1.4.1.%s.1.2.0.%s_%s", strtok(fdfValues.studyid,"\""), strtok(fdfValues.oper,"\""),strtok(fdfValues.pname,"\""));
+		 snprintf(studyUID, sizeof(studyUID), "1.3.6.1.4.1.%s.1.2.0.%s_%s", strtok(fdfValues.studyid,"\""), strtok(fdfValues.oper,"\""), strtok(fdfValues.pname,"\""));
 
 		list+=new UIStringAttribute(TagFromName(StudyInstanceUID), studyUID);
 
@@ -713,6 +718,7 @@ int main(int argc,char **argv)
 
 		// Calculations are done differently depending on whether we're converting a
 		// 2D acquisition or a 3D acquisition.
+		float ctr[3] = {0.0f, 0.0f, 0.0f};
 		if(fdfValues.rank == 2)
 		{
 			// 1) Figure out distance from center of image to upper left corner:
@@ -889,13 +895,20 @@ int main(int argc,char **argv)
 		// If we found the file, process the custom tags
 		if(haveTagFile)
 		{
-		  ProcessCustomTags(customtagPath, procparPath, fdf_fd, list);
+		  if(fdf_fd != NULL)
+		  {  
+			  ProcessCustomTags(customtagPath, procparPath, fdf_fd, list);
+		  }
 		}
 		else
 		{
 			printf("Custom tag file not found, ignoring...\n");
 		}
-		fclose(fdf_fd); // now done with fdf file
+		if(fdf_fd != NULL)
+		{
+			fclose(fdf_fd); // now done with fdf file
+			fdf_fd = NULL;
+		}
 
 		// Add procpar file as a custom tag
 //		OtherByteLargeNonPixelAttribute(Tag t,BinaryInputStream &stream,OurStreamPos pos)
@@ -993,25 +1006,27 @@ static void makesha1uid(char *filepath, char *filename, char *uidbuf)
 {
   int i,j;
   char sha1Path[256];
-  char fpath[256];
-  char sha1[NSHA1];
-  char   syscmd[256];
+  char fpath[512];
+  char sha1[NSHA1 + 1];
+  char   syscmd[800];
   FILE *fsha;
 
   // get its sha1sum to use for UID
-  (void)strcpy(sha1Path,"/vnmr/tmp");
-  (void)strcat(sha1Path,"/sha1.txt");
+  (void)snprintf(sha1Path, sizeof(sha1Path), "/vnmr/tmp/sha1.txt");
 
-  (void)strcpy(fpath,filepath);
-  (void)strcat(fpath,"/");
-  (void)strcat(fpath,filename);
+  (void)snprintf(fpath, sizeof(fpath), "%s/%s", filepath, filename);
 
-  (void)sprintf(syscmd,"sha1sum %s > %s \n",fpath,sha1Path);
+  (void)snprintf(syscmd, sizeof(syscmd), "sha1sum %s > %s \n", fpath, sha1Path);
   (void)system(syscmd);
 
   //read it in
   fsha=fopen(sha1Path,"r");
-  if(!fsha)printf("Could not open sha1 for %s \n",fpath);
+  if(!fsha)
+  {
+	  printf("Could not open sha1 for %s \n",fpath);
+	  uidbuf[0] = '\0';
+	  return;
+  }
   fscanf(fsha,"%10s",sha1);
   (void)fclose(fsha);
   unlink(sha1Path);
